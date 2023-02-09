5808006e23ce8a4f64694ad51e19f613dd2ceb44
Caio Andrade <candrade@infotech.com>
2023-02-09 08:58:35 -0600

    Update form component

--- app/components/search/form_component.rb
+++ app/components/search/form_component.rb
@@ -1,7 +1,10 @@
 module Search
   class FormComponent < ViewComponent::Base
-    include ActiveModel::Model
+    attr_reader :search
 
-    attr_accessor :name, :species
+    def initialize(search)
+      @search = search
+      super
+    end
   end
 end
